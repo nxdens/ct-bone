e86f58b5ec49338c0fde631def5ec855e9ae7dec
nxdens <>
2018-08-15 15:18:49 -0400

    fixed the axis issue

--- itk/ImageReader.cxx
+++ itk/ImageReader.cxx
@@ -8,6 +8,8 @@
 #include <vtkImageViewer2.h>
 #include <vtkRenderWindowInteractor.h>
 
+#include <vtkImageFlip.h>
+
 int main(int argc, char *argv[])
 {
 	if(argc < 2)
@@ -16,19 +18,25 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	typedef itk::Image< itk::RGBPixel<unsigned short> ,3>	ImageType;
+	typedef itk::Image< itk::RGBPixel<unsigned char> ,2>	ImageType;
 	typedef itk::ImageFileReader<ImageType> ReaderType;
 
 	ReaderType::Pointer reader = ReaderType::New();
 	reader->SetFileName(argv[1]);
-
+	//This doesnt convert the coordinate systems so the image that you get from vtk is flipped in the y from itk 
 	typedef itk::ImageToVTKImageFilter<ImageType> ConnectorType;
 
 	ConnectorType::Pointer connector = ConnectorType::New();
 	connector->SetInput(reader->GetOutput());
 	
+	//*fixes* coordinate system problem
+	vtkSmartPointer<vtkImageFlip> flipY = vtkSmartPointer<vtkImageFlip>::New();
+	flipY->SetFilteredAxis(1);
+	flipY->SetInputData(connector->GetOutput());
+	flipY->Update();
+
 	vtkSmartPointer<vtkImageViewer2> viewer = vtkSmartPointer<vtkImageViewer2>::New();
-	viewer->SetInputData(connector->GetOutput());
+	viewer->SetInputConnection(flipY->GetOutputPort());
 	connector->Update();
 	viewer->GetRenderer()->ResetCamera();
 
