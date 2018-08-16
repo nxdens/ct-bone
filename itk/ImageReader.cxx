0ebd4ff508cd97246a623e556b338fce9f758abe
nxdens <>
2018-08-16 13:33:50 -0400

    added the 3d volume rendering with itk reading the file there doesnt seem to be an issue with flipping the y axis with this

--- itk/ImageReader.cxx
+++ itk/ImageReader.cxx
@@ -6,10 +6,18 @@
 #include <vtkSmartPointer.h>
 #include <vtkRenderer.h>
 #include <vtkImageViewer2.h>
+#include <vtkInteractorStyleTrackballCamera.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkRenderWindow.h>
+#include <vtkVolume.h>
+#include <vtkVolumeProperty.h>
+#include <vtkVolumeMapper.h>
+#include <vtkSmartVolumeMapper.h>
+#include <vtkImageData.h>
+#include <vtkPiecewiseFunction.h>
 
 #include <vtkImageFlip.h>
-
+#include "QuickView.h"
 int main(int argc, char *argv[])
 {
 	if(argc < 2)
@@ -18,19 +26,64 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	typedef itk::Image< itk::RGBPixel<unsigned char> ,2>	ImageType;
+	typedef itk::Image< unsigned short ,3>	ImageType;
 	typedef itk::ImageFileReader<ImageType> ReaderType;
 
 	ReaderType::Pointer reader = ReaderType::New();
 	reader->SetFileName(argv[1]);
+	reader->Update();
 	//This doesnt convert the coordinate systems so the image that you get from vtk is flipped in the y from itk 
+
+	ImageType::Pointer image = reader->GetOutput();
+    image->Print(std::cout ); 
+	
+	
+
+	vtkSmartPointer<vtkRenderWindow> window = vtkSmartPointer<vtkRenderWindow>::New();
+	vtkSmartPointer<vtkRenderer> render = vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	vtkSmartPointer<vtkVolume> volume = vtkSmartPointer<vtkVolume>::New();
+	vtkSmartPointer<vtkSmartVolumeMapper> mapper = vtkSmartPointer<vtkSmartVolumeMapper>::New();
+	vtkSmartPointer<vtkVolumeProperty> prop = vtkSmartPointer<vtkVolumeProperty>::New();
+	vtkSmartPointer<vtkImageData> data = vtkSmartPointer<vtkImageData>::New();
+	vtkSmartPointer<vtkInteractorStyleTrackballCamera> style = vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+
+
+	window->AddRenderer(render);
+	window->SetSize(500,500);
+	interactor->SetInteractorStyle(style);
+	interactor->SetRenderWindow(window);
+	window->Render();
+
 	typedef itk::ImageToVTKImageFilter<ImageType> ConnectorType;
 
 	ConnectorType::Pointer connector = ConnectorType::New();
-	connector->SetInput(reader->GetOutput());
-	
+	connector->SetInput(image);
+	connector->Update();
+
+	data->ShallowCopy(connector->GetOutput());
+
+	mapper->SetBlendModeToComposite();
+	mapper->SetRequestedRenderModeToGPU();
+	mapper->SetInputData(data);
+
+	vtkSmartPointer<vtkPiecewiseFunction> compositeOpacity =
+	vtkSmartPointer<vtkPiecewiseFunction>::New();
+	compositeOpacity->AddPoint(0.0,0.0);
+	compositeOpacity->AddPoint(100.0,1);
+	compositeOpacity->AddPoint(255.0,1);
+	prop->SetScalarOpacity(compositeOpacity);
+
+	volume->SetMapper(mapper);
+	volume->SetProperty(prop);
+
+	render->AddVolume(volume);
+	render->ResetCamera();
+
+	window->Render();
+	interactor->Start();
 	//*fixes* coordinate system problem
-	vtkSmartPointer<vtkImageFlip> flipY = vtkSmartPointer<vtkImageFlip>::New();
+	/*vtkSmartPointer<vtkImageFlip> flipY = vtkSmartPointer<vtkImageFlip>::New();
 	flipY->SetFilteredAxis(1);
 	flipY->SetInputData(connector->GetOutput());
 	flipY->Update();
@@ -44,7 +97,7 @@ int main(int argc, char *argv[])
 	
 	viewer->SetupInteractor(interactor);
 	interactor->Initialize();
-	interactor->Start();
+	interactor->Start();*/
 
 	return 0;
 }
