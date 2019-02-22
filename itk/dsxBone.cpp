b3bc8c34b71b2e320cc6bc8fd8f46b452b039c0c
nxdens <qyaoz@hotmail.com>
2019-02-22 12:41:56 -0500

    Added most of the vtkVolume setup inside bone

--- itk/dsxBone.cpp
+++ itk/dsxBone.cpp
@@ -2,32 +2,56 @@
 
 dsxBone::dsxBone() 
 {
+	currentPose = new dsxPose();
+	mCtBoneVoxels = vtkSmartPointer<vtkVolume>::New();
+	mCtBoneMapper = vtkSmartPointer<vtkSmartVolumeMapper>::New();
+	mCtImageData = vtkSmartPointer<vtkImageData>::New();
+	mCtCompositeOpacity = vtkSmartPointer<vtkPiecewiseFunction>::New();
+	mCtBoneVoxels->SetMapper(mCtBoneMapper);
+	mCtBoneMapper->SetRequestedRenderModeToGPU();
+	mCtCompositeOpacity->AddPoint(99,0.0);
+	mCtCompositeOpacity->AddPoint(100.0,1);
+	mInternalTransformMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
 }
-
+dsxBone::dsxBone(int totalFrames)
+{
+	mTotalFrames = totalFrames;
+	dsxBone();
+}
 dsxBone::~dsxBone()
 {}
 
+void dsxBone::setBone(vtkImageData * ctImageData)
+{
+	mCtImageData->DeepCopy(ctImageData);
+	mCtBoneMapper->SetInputData(mCtImageData);
+	mCtBoneVoxels->GetProperty()->SetScalarOpacity(mCtCompositeOpacity);
+	//maybe get the initial position from this?
+}
+
 void dsxBone::rotate(double roll,double yaw,double pitch)
 {
-	this->currentPose->rotate(roll,yaw,pitch);
+	currentPose->rotate(roll,yaw,pitch);
 	mCtBoneVoxels->RotateX(roll);
 	mCtBoneVoxels->RotateY(yaw);
 	mCtBoneVoxels->RotateZ(pitch);
 	//mCtBoneVoxels->AddOrientation(roll, yaw, pitch)
 }
-void dsxBone::translate(double x,double y,double z)
+void dsxBone::translate(double translateX,double translateY,double translateZ)
 {
-	mCtBoneVoxels->AddPosition(x,y,z);
-	currentPose->translate(x,y,z);
+	mCtBoneVoxels->AddPosition(translateX,translateY,translateZ);
+	currentPose->translate(translateX,translateY,translateZ);
 }
-void dsxBone::scale(double c)
+void dsxBone::scale(double scaleFactor)
 {
-	mCtBoneVoxels->SetScale(c);
-	currentPose->scale(c);
+	mCtBoneVoxels->SetScale(scaleFactor);
+	currentPose->scale(scaleFactor);
 }
 dsxPose dsxBone::recordPose()
 {
 	oldPose = *currentPose;
+	mCtBoneVoxels->ComputeMatrix();
+	mCtBoneVoxels->GetMatrix(mInternalTransformMatrix);
 	return oldPose;
 }
 void dsxBone::restorePose()
@@ -43,4 +67,8 @@ dsxPose * dsxBone::applyPose(dsxPose newPose)
 	oldPose = *currentPose;
 	*currentPose = newPose;
 	return &oldPose;
+}
+void dsxBone::saveCurrentPose()
+{
+	poseSequence.push_back(*currentPose);
 }
--- itk/dsxBone.h
+++ itk/dsxBone.h
@@ -1,32 +1,45 @@
 #ifndef __DSX_BONE__
 #define __DSX_BONE__
-
+#include <vtkSmartPointer.h>
 #include <vtkVolume.h>
 #include <vtkVolumeProperty.h>
 #include <vtkVolumeMapper.h>
 #include <vtkSmartVolumeMapper.h>
+#include <vtkPiecewiseFunction.h>
+#include <vtkImageData.h>
+#include <vtkMatrix4x4.h>
 #include "dsxPose.h"
 #include <iostream>
+#include <list>
 class dsxBone
 {
 public:
+	dsxPose * applyPose(dsxPose newPose);
+	dsxPose * getPose();
+	dsxPose * currentPose;
+	dsxPose oldPose;
+	vtkVolume * mCtBoneVoxels;
+	vtkSmartVolumeMapper * mCtBoneMapper;
+	vtkImageData * mCtImageData;
+	std::list <dsxPose> poseSequence;
+	vtkPiecewiseFunction* mCtCompositeOpacity;
+	vtkMatrix4x4 * mInternalTransformMatrix;
+	int mTotalFrames = 0;
 	dsxBone();
+	dsxBone(int totalFrames);
 	~dsxBone();
 	static dsxBone * New()
 	{
 		dsxBone *ctBone = new dsxBone();
 		return ctBone;
 	}
+	void setBone(vtkImageData * ctImageData);
 	void rotate(double roll,double yaw,double pitch);
 	void translate(double translateX,double translateY,double translateZ);
-	void scale(double c); //uniform scaling
+	void scale(double scaleFactor); //uniform scaling
 	dsxPose recordPose();
 	void restorePose();
-	dsxPose * applyPose(dsxPose newPose);
-	dsxPose * getPose();
-	dsxPose * currentPose;
-	dsxPose oldPose;
-	vtkVolume * mCtBoneVoxels;
-	//std::list <dsxPose> poseSequence;
+	void saveCurrentPose();
+	void setPosition(double translateX,double translateY,double translateZ);
 };
 #endif
